4406ca4760528981a754315b9c3b2066b89323b8
Mar1aLN <157271142+Mar1aLN@users.noreply.github.com>
2024-01-31 19:27:02 +0500

    Delete src/test/java/constants directory

--- src/test/java/constants/Url.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class Url {
-    public final static String QA_SCOOTER_URL = "https://qa-scooter.praktikum-services.ru/";
-    public final static String QA_SCOOTER_ORDER_URL = "https://qa-scooter.praktikum-services.ru/order";
-}
